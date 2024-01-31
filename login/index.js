3d0559c7a89b91aba7131d4a925ae5756f20b5d0
Alexis Calvario <Brandonalexiscc>
2024-01-30 23:30:26 -0600

    avance Multi-step Register step 2

--- login/index.js
+++ login/index.js
@@ -1,7 +1,13 @@
-function next(){
-    let myBtn = document.getElementById('submir');
+function mostrarAlerta(){
+    let user = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-    myBtn.addEventListener('click', function(){
-        
+    if ( user == '' || email == ''){
+        alert('Completa los campos vacios');
+    } else if (!emailRegex.test(email)){
+        alert('Favor de ingresar un correo valido');
     }
+    let nuevaVista = document.getElementById('sumbit');
+    window.location.href = 'chooseTopics.html';
 }
